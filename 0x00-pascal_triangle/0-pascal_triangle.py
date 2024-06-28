4ae8fdaa180eca7dcb54fec5f00dffabd4c96bad
foo <foo@bar.com>
2024-06-28 07:23:48 +0100

    updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,22 +9,22 @@ def pascal_triangle(n):
   """
   triangle = []
   i = 0
-    
+  
   if n <= 0:
     return []
-        
-  while i < n :
-    row = [0] * (i + 1)
-    row[0] = 1
-    row[-1] = 1
-        
-    if i > 1:
-        j = 1
-        while j < i:
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-            j += 1
-        
-    triangle.append(row)
-    i += 1
+  else:      
+    while i < n :
+        row = [0] * (i + 1)
+        row[0] = 1
+        row[-1] = 1
+            
+        if i > 1:
+            j = 1
+            while j < i:
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                j += 1
+            
+        triangle.append(row)
+        i += 1
          
     return triangle
