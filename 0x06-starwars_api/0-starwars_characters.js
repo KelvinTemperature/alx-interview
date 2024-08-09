bfc0fa13999e9f0807edd34c49fb9b941c90afdf
Kelvin <koolkt10@gmail.com>
2024-08-09 02:11:00 +0000

    order corrected

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const url = 'https://swapi-api.alx-tools.com/films/' + process.argv[2] + '/';
-
-request(url, (err, response, data) => {
-    if (err) throw err;
-
-    const films = JSON.parse(data);
-
-    films.characters.forEach((value) => {
-        request(value, (err, response, data) => {
-            if (err) throw err;
-
-            const character = JSON.parse(data);
-            console.log(character.name);
-        });
-    });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
